5d1086c4fa2c9c74f283c548439e043dedc8b313
chomupdding14 <h_daichi0423@yahoo.co.jp>
2025-08-11 18:44:48 -0700

    Delete assets directory

--- assets/js/main.js
+++
@@ -1,6 +0,0 @@
-(function(){
-  const btn = document.getElementById('menuBtn');
-  const menu = document.getElementById('mobileMenu');
-  if (btn && menu) btn.addEventListener('click', () => menu.classList.toggle('hidden'));
-  const y = document.getElementById('year'); if (y) y.textContent = new Date().getFullYear();
-})();
